5cd24124475d7790af1feed4c4ae70a2b81f164f
Harsh <harsh.techilia@gmail.com>
2024-03-21 01:05:41 +0530

    refactor telegram bot

--- src/redis/redisBot.js
+++ src/redis/redisBot.js
@@ -1,12 +1,11 @@
 const TelegramBot = require("node-telegram-bot-api");
 const redis = require("redis");
 const nodemailer = require("nodemailer");
+const { validEmail } = require("../utils");
 
-// Initialize bot
 const TOKEN = "7023229435:AAH9Eh0wJi3fT1K-JFU7NTAmSMZ2qFtkL94";
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-// Initialize Redis client
 const client = redis.createClient({
 	password: "02H21j9N0zNO3M1mmWsMFAo006HfTln8",
 	socket: {
@@ -19,29 +18,17 @@ client.on("error", (err) => console.log("Redis Client Error", err));
 
 (async () => {
 	const conn = await client.connect();
-	console.log(conn);
 })();
-// connection();
 
-// Function to generate a random verification code
-function generateVerificationCode() {
-	return Math.random().toString(36).substring(2, 8); // Generate a random 6-character alphanumeric code
-}
-
-// Function to send email with verification code
 async function sendVerificationEmail(email, code) {
-	// Create a transporter object
-
-	console.log(email);
 	const transporter = nodemailer.createTransport({
 		service: "Gmail",
 		auth: {
-			user: "mohammadabbas434@gmail.com", // Add your Gmail email address
-			pass: "gpzk emcb xgxa cdqo", // Add your Gmail password (2 step verification passcode)
+			user: "mohammadabbas434@gmail.com",
+			pass: "gpzk emcb xgxa cdqo",
 		},
 	});
 
-	// Setup email data
 	const mailOptions = {
 		from: "mohammadabbas434@gmail.com",
 		to: email,
@@ -49,7 +36,6 @@ async function sendVerificationEmail(email, code) {
 		text: `Your verification code is: ${code}`,
 	};
 
-	//send email
 	try {
 		await transporter.sendMail(mailOptions);
 		console.log("Verification email sent successfully.");
@@ -58,126 +44,97 @@ async function sendVerificationEmail(email, code) {
 	}
 }
 
-// Handle /start command
 bot.onText(/\/start/, async (msg) => {
 	const chatId = msg.chat.id;
 
-	console.log({ chatId });
-
-	// Send welcome message
 	bot.sendMessage(
 		chatId,
 		"Welcome to the bot! Please enter your email address:"
 	);
 });
 
-// Handle user's email input
-bot.on("message", async (msg) => {
-	console.log({ msg });
+bot.onText(/\/start/, async (msg) => {
 	const chatId = msg.chat.id;
 	const messageText = msg.text;
 
-	console.log({ messageText });
+	if (validEmail(msg.text)) {
+		const verificationCode = Math.floor(
+			100000 + Math.random() * 900000
+		).toString();
 
-	// Check if the message is an email address
-	// if (/^\S+@\S+\.\S+$/.test(messageText))
+		// Save email and verification code to Redis
+		await client.set(
+			chatId.toString(),
+			JSON.stringify({
+				email: messageText,
+				verificationCode: verificationCode,
+				verified: false,
+			})
+		);
 
-	// Generate verification code
-	const verificationCode = Math.floor(
-		100000 + Math.random() * 900000
-	).toString();
+		// Send verification email
+		sendVerificationEmail(messageText, verificationCode);
 
-	// Save email and verification code to Redis
-	await client.set(
-		chatId.toString(),
-		JSON.stringify({
-			email: messageText,
-			verificationCode: verificationCode,
-			verified: false,
-		})
-	);
-
-	// Send verification email
-	sendVerificationEmail(messageText, verificationCode);
-
-	// Prompt the user to enter the verification code
-	bot.sendMessage(
-		chatId,
-		"Verification code sent to your email. Please enter the code:"
-	);
-	// } else {
-	// 	// Prompt the user to enter a valid email address
-	// 	bot.sendMessage(chatId, "Please enter a valid email address.");
-	// }
-});
-
-// Handle verification code input
-bot.on("message", async (msg) => {
-	const chatId = msg.chat.id;
-	const messageText = msg.text;
-
-	// Check if the message is a 6-digit verification code
-	if (/^\d{6}$/.test(messageText)) {
-		// Get email and verification code from Redis
-		client.get(chatId.toString(), (err, data) => {
-			if (err) {
-				console.error("Error retrieving data from Redis:", err);
-				return;
-			}
-
-			if (data && data.verificationCode === messageText) {
-				console.log({ data });
-				// Mark user as verified in Redis
-				// client.hSet("chatId", "verified", true);
-
-				client.hDel(chatId);
-
-				// Send congratulations message
-				bot.sendMessage(chatId, "Congratulations, you are verified!");
-
-				// Show options for the user to continue using the bot
-				bot.sendMessage(chatId, "What would you like to do?", {
-					reply_markup: {
-						keyboard: [
-							[{ text: "Option 1" }, { text: "Option 2" }],
-							[{ text: "Option 3" }, { text: "Option 4" }],
-						],
-						resize_keyboard: true,
-					},
-				});
-			} else {
-				// Notify the user about incorrect verification code
-				bot.sendMessage(
-					chatId,
-					"Incorrect verification code. Please try again."
-				);
-			}
-		});
+		// Prompt the user to enter the verification code
+		bot.sendMessage(
+			chatId,
+			"Verification code sent to your email. Please enter the code:"
+		);
 	}
 });
 
-// Handle button clicks
-bot.on("message", (msg) => {
+bot.on("message", async (msg) => {
 	const chatId = msg.chat.id;
 	const messageText = msg.text;
 
-	console.log({ messageText });
+	if (validEmail(msg.text)) {
+		const verificationCode = Math.floor(
+			100000 + Math.random() * 900000
+		).toString();
 
-	// Handle button clicks based on the message text
-	switch (messageText) {
-		case "Option 1":
-			bot.sendMessage(chatId, "You clicked Option 1.");
-			break;
-		case "Option 2":
-			bot.sendMessage(chatId, "You clicked Option 2.");
-			break;
-		case "Option 3":
-			bot.sendMessage(chatId, "You clicked Option 3.");
-			break;
-		case "Option 4":
-			bot.sendMessage(chatId, "You clicked Option 4.");
-			break;
-		default:
-			console.log("Ignoring message:", messageText);
+		await client.set(
+			chatId.toString(),
+			JSON.stringify({
+				email: messageText,
+				verificationCode: verificationCode,
+				verified: false,
+			})
+		);
+
+		sendVerificationEmail(messageText, verificationCode);
+
+		bot.sendMessage(
+			chatId,
+			"Verification code sent to your email. Please enter the code:"
+		);
+	}
+
+	if (/^\d{6}$/.test(messageText)) {
+		const data = await client.get(chatId.toString());
+		const parsedData = JSON.parse(data);
+		if (parsedData?.verificationCode == messageText.toString()) {
+			const userData = {
+				chatId: chatId,
+				first_name: msg.from.first_name,
+				last_name: msg.from.last_name,
+				username: msg.from.username,
+				email: parsedData.email,
+			};
+			client.del(chatId.toString());
+
+			bot.sendMessage(chatId, "Congratulations, you are verified!");
+
+			bot.sendMessage(chatId, "What would you like to do?", {
+				reply_markup: {
+					keyboard: [
+						[{ text: "Option 1" }, { text: "Option 2" }],
+						[{ text: "Option 3" }, { text: "Option 4" }],
+					],
+					resize_keyboard: true,
+				},
+			});
+		} else {
+			bot.sendMessage(chatId, "Incorrect verification code. Please try again.");
+		}
 	}
 });
---
+++ src/utils.js
@@ -0,0 +1,6 @@
+function validEmail(email) {
+	const re = /\S+@\S+\.\S+/;
+	return re.test(email);
+}
+
+module.exports = { validEmail };
